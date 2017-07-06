687d2809e165e7709fad075dbd5fd6e1eaffeee0
Achille Verheye <achille.verheye@gmail.com>
2017-07-05 22:15:04 -0400

    fixing relative path crap in dynamixel_functions.py

--- python/dynamixel_functions_py/dynamixel_functions.py
+++ python/dynamixel_functions_py/dynamixel_functions.py
@@ -33,12 +33,16 @@
 
 # Author: Ryu Woon Jung (Leon)
 
+# figure out the path of the directory for THIS file, because Rya Woon Jung is dumb.
+import os
+cwd = os.path.dirname(__file__)
+
 from ctypes import cdll
 # dxl_lib = cdll.LoadLibrary("../../c/build/win32/output/dxl_x86_c.dll")  # for windows 32bit
 # dxl_lib = cdll.LoadLibrary("../../c/build/win64/output/dxl_x64_c.dll")  # for windows 64bit
 # dxl_lib = cdll.LoadLibrary("../../c/build/linux32/libdxl_x86_c.so")     # for linux 32bit
 # dxl_lib = cdll.LoadLibrary("../../c/build/linux64/libdxl_x64_c.so")     # for linux 64bit
-dxl_lib = cdll.LoadLibrary("../../c/build/linux_sbc/libdxl_sbc_c.so")   # for SBC linux
+dxl_lib = cdll.LoadLibrary(cwd+"/../../c/build/linux_sbc/libdxl_sbc_c.so")   # for SBC linux
 
 # port_handler
 portHandler = dxl_lib.portHandler
